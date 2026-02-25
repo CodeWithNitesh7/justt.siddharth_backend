30d6a91e6ba332bd905bf66878f4e00c2db16b92
Nitesh Gupta <niteshgupta0985@gmail.com>
2026-02-25 10:35:07 +0530

    Refactor CORS options and improve connection handling

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./src/config/db');
 
+connectDB();
 const app = express();
 const PORT = process.env.PORT;
-const Origin = ['https://codexnitesh.vercel.app',
-               'https://codewithnitesh.vercel.app',
-               'https://CodeWithNitesh.vercel.app'];
-const Options={
-    origin:"*",
-    methods:['GET','PUT','PATCH','DELETE','POST'],
-    credentials:true
+const Origin = [
+  'https://codexnitesh.vercel.app',
+  'https://codewithnitesh.vercel.app',
+  'https://CodeWithNitesh.vercel.app'
+];
 
-}
-
+const Options = {
+  origin: function (origin, callback) {
+    if (!origin || Origin.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'PUT', 'PATCH', 'DELETE', 'POST'],
+  credentials: true
+};
 // Connecting database
-connectDB();
+app.use(cors(Options));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(Options));
 // routers 
 app.use('/auth',require('./src/routes/auth.routes'))
 app.use('/projects',require('./src/routes/projects.routes'))
